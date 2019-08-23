105744fe00b1cd3bedc7fc3f9361da5fa0195b94
kris <418926326@qq.com>
2019-08-23 09:55:48 +0800

    第一次添加op模块

---
+++ helloworld/src/main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  print "hello world" every 5 second
+ *                  
+ *
+ *        Version:  1.0
+ *        Created:  02/22/2016 03:56:43 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  star , star.jiang@siflower.com.cn
+ *        Company:  Siflower Communication Tenology Co.,Ltd
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+void main()
+{
+	while(1){
+		printf("_____________hello kris！！！\n");
+		sleep(5);
+	}
+}
